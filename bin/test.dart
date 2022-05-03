918fc7991c68f72dce0fe8541a3e909a827cb48a
h80r <h80rgbc@gmail.com>
2022-05-03 16:10:22 -0300

    Implement local testing

---
+++ bin/test.dart
@@ -0,0 +1,52 @@
+import 'package:test/test.dart';
+
+import 'functions/expression_conversion.dart';
+
+void main() {
+  group('[NA] Revisão de expressões regulares:', () {
+    test('(a+b)*(aa+bb)', () {
+      expect(infixToPostfix('(a+b)*(aa+bb)'), 'ab+*aa.bb.+.');
+    });
+    test('(a*b*)* ', () {
+      expect(infixToPostfix('(a*b*)*'), 'a*b*.*');
+    });
+    test('(a+b)(c+d)', () {
+      expect(infixToPostfix('(a+b)(c+d)'), 'ab+cd+.');
+    });
+    test('(b*ab*ab*)(b*ab*ab*)*', () {
+      expect(infixToPostfix('(b*ab*ab*)(b*ab*ab*)*'),
+          'b*a.b*.a.b*.b*a.b*.a.b*.*.');
+    });
+  });
+
+  group('[NA] De notação infixa para posfixa:', () {
+    test('((aa)* (ab+ba)(bb)*a(a+b))*', () {
+      expect(infixToPostfix('((aa)* (ab+ba)(bb)*a(a+b))*'),
+          'aa.*ab.ba.+.bb.*.a.ab+.*');
+    });
+    test('(5 + 9) * 2 + 6 * 5', () {
+      expect(infixToPostfix('(5 + 9) * 2 + 6 * 5'), '59+*2.6*5.+');
+    });
+    test('( A + B ) * ( C . D )', () {
+      expect(infixToPostfix('( A + B ) * ( C . D )'), 'AB+*CD..');
+    });
+    test('A + B * C', () {
+      expect(infixToPostfix('A + B * C'), 'AB*C.+');
+    });
+    test('( A + B ) * C', () {
+      expect(infixToPostfix('( A + B ) * C'), 'AB+*C.');
+    });
+  });
+
+  group('[A] Testes de parênteses:', () {
+    test('((aa) * (ab)+(ba)((bb)*a(a+b))', () {
+      expect(infixToPostfix('((aa) * (ab)+(ba)((bb)*a(a+b))'), null);
+    });
+    test('aa()', () {
+      expect(infixToPostfix('aa()'), 'aa..');
+    });
+    test('a)(b', () {
+      expect(infixToPostfix('a)(b'), null);
+    });
+  });
+}
