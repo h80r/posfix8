7bd867ef77086e3e6bb9a8d3fd2025792a6210a2
h80r <h80rgbc@gmail.com>
2022-05-24 13:46:34 -0300

    Remove flawed graph visualization

--- client/lib/canvas/graph.dart
+++ client/lib/canvas/graph.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:graphview/GraphView.dart';
 
 import 'package:client/provider/automata.dart';
 import 'package:client/provider/canvas/graph.dart';
 
 import 'components/automata_table.dart';
 import 'components/cart_title.dart';
-import 'components/core/graph_dot.dart';
 import 'components/core/styled_card.dart';
 
 class GraphCanvas extends ConsumerWidget {
@@ -17,34 +15,13 @@ class GraphCanvas extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final graph = ref.watch(GraphNotifier.provider);
-    final colors = ref.watch(GraphNotifier.provider.notifier).colorMap;
-
+    final colors = ref.watch(TransitionNotifier.provider);
     final automata = ref.watch(automataProvider);
 
     return Scaffold(
       appBar: AppBar(title: const Text('Visualização de Autômato')),
       body: Column(
         children: [
-          Expanded(
-            child: StyledCard(
-              child: InteractiveViewer(
-                boundaryMargin: const EdgeInsets.all(8),
-                constrained: false,
-                minScale: 0.001,
-                maxScale: 100,
-                child: GraphView(
-                  graph: graph,
-                  algorithm: SugiyamaAlgorithm(
-                    SugiyamaConfiguration()
-                      ..levelSeparation = 50
-                      ..nodeSeparation = 50,
-                  ),
-                  builder: (Node node) => GraphDot(node),
-                ),
-              ),
-            ),
-          ),
           Expanded(
             child: StyledCard(
               child: Column(
--- client/lib/provider/canvas/graph.dart
+++ client/lib/provider/canvas/graph.dart
@@ -2,76 +2,41 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:graphview/GraphView.dart';
 
 import '../automata.dart';
 
-class GraphNotifier extends StateNotifier<Graph> {
-  GraphNotifier(
-    this._automata,
-    this._colorMap,
-    this._kGoldenAngle,
-  ) : super(Graph()) {
+class TransitionNotifier extends StateNotifier<Map<String, Color>> {
+  TransitionNotifier(this._automata, this._kGoldenAngle) : super({}) {
     loadAutomata();
   }
 
   final Map<String, dynamic> _automata;
-  final Map<String, Color> _colorMap;
   final double _kGoldenAngle;
 
-  static final provider = StateNotifierProvider<GraphNotifier, Graph>((ref) {
-    return GraphNotifier(ref.watch(automataProvider), {}, 180 * (3 - sqrt(5)));
+  static final provider =
+      StateNotifierProvider<TransitionNotifier, Map<String, Color>>((ref) {
+    return TransitionNotifier(ref.watch(automataProvider), 180 * (3 - sqrt(5)));
   });
 
   void loadAutomata() {
-    final newGraph = Graph();
-
-    final states = _automata['states'] as List<String>;
     final alphabet = _automata['alphabet'] as List<String>;
-    final table = _automata['table'] as List<List<String>>;
 
-    final nodes = states.map(Node.Id).toList();
-    newGraph.addNodes(nodes);
-
-    final edges = <Edge>[];
-    for (var i = 0; i < states.length; i++) {
-      for (var j = 0; j < alphabet.length; j++) {
-        final target = table[i][j];
-
-        final from = nodes[i];
-        final symbol = alphabet[j];
-
-        final toNode = nodes.firstWhere((e) => e.key?.value == target);
-        edges.add(Edge(
-          from,
-          toNode,
-          paint: Paint()
-            ..color = _getColor(symbol)
-            ..strokeWidth = 2.0,
-        ));
-      }
+    for (var j = 0; j < alphabet.length; j++) {
+      final symbol = alphabet[j];
+      _setColor(symbol);
     }
-
-    newGraph.addEdges(edges);
-
-    state = newGraph;
   }
 
-  Color _getColor(String id) {
-    if (_colorMap.containsKey(id)) return _colorMap[id]!;
+  void _setColor(String id) {
+    if (state.containsKey(id)) return;
 
-    final color = id == 'ε'
-        ? Colors.white.withOpacity(0.5)
-        : HSLColor.fromAHSL(
-            1.0,
-            (_colorMap.length * _kGoldenAngle) % 360.0,
-            1.0,
-            0.75,
-          ).toColor();
+    final color = HSLColor.fromAHSL(
+      1.0,
+      (state.length * _kGoldenAngle) % 360.0,
+      1.0,
+      0.75,
+    ).toColor();
 
-    _colorMap[id] = color;
-    return color;
+    state = {...state, id: color};
   }
-
-  Map<String, Color> get colorMap => {..._colorMap};
 }
